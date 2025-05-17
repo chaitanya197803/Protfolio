5202fa906e997bf52063a3172ff0ca9f1a2a97f3
chaitanya197803 <cgarg9432@gmail.com>
2025-05-17 20:41:53 +0530

    first commit

---
+++ src/app/component/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <section>
+            <h1 className='text-white mb-4 text-6xl  font-extrabold'>Hello I'm Chaitanya</h1>
+            <p className='text-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem quaerat ut commodi aspernatur sed quisquam ratione alias obcaecati! Perferendis.
+            </p>
+        </section> 
+    </div>
+  )
+}
+
+export default HeroSection

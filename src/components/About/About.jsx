1918f33cf281b6c2c671183023cb9112e628549b
chaitanya197803 <cgarg9432@gmail.com>
2025-07-13 16:24:36 +0530

    about typing effect

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactTypingEffect from 'react-typing-effect'
+import Typewriter from 'typewriter-effect';
 
 const About = () => {
   return (
@@ -18,13 +19,24 @@ const About = () => {
           <h2 className='text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight'>
             Chaitanya Garg
           </h2>
+
           {/* skills hedding with typing effect */}
           <h3 className='text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight'>
-            <span className='text-white'>
-              I am a <span className='text-[#8245ec]'>Software Engineer</span>
+            <span className='flex text-white'>
+              I am a 
+              <span className="flex text-[#8245ec]">
+                <p>&nbsp;</p>
+              <Typewriter
+                options={{
+                  strings: ['React Developer', 'UI/UX Designer', 'Freelancer'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
             </span>
-            
-          {/* <ReactTypingEffect text={['Web Devloper','Frontend Developer','Backend Developer',]}
+            </span>
+
+            {/* <ReactTypingEffect text={['Web Devloper','Frontend Developer','Backend Developer']}
           speed={100}
           eraseSpeed={50}
           typingDelay={500}
@@ -34,16 +46,18 @@ const About = () => {
           )}
           ></ReactTypingEffect> */}
 
+            
           </h3>
 
           {/* About me Paragraph  */}
           <p className='text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptas
-             minus voluptates cupiditate eligendi nostrum. Quisquam voluptate ipsa iusto temporibus
-              debitis, sunt obcaecati laboriosam facilis similique cupiditate maxime recusandae praesentium 
-              aspernatur, numquam ipsum dolore earum pariatur nobis repellendus. Dolores commodi similique 
-              laborum iusto vel dicta quae iure voluptatibus exercitationem inventore!
+            minus voluptates cupiditate eligendi nostrum. Quisquam voluptate ipsa iusto temporibus
+            debitis, sunt obcaecati laboriosam facilis similique cupiditate maxime recusandae praesentium
+            aspernatur, numquam ipsum dolore earum pariatur nobis repellendus. Dolores commodi similique
+            laborum iusto vel dicta quae iure voluptatibus exercitationem inventore!
           </p>
+
         </div>
       </div>
     </section>

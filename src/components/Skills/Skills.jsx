6b640fc0533180ca6e2829f964ea9ccef25ffb74
chaitanya197803 <cgarg9432@gmail.com>
2025-07-17 10:42:45 +0530

    skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { SkillsInfo } from '../../../src/constant'
 const Skills = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+    id='skills'
+    className='py-24 pb-24 px-[12vw] md:px-[7vw] lg:px[20vw] font-sans bg-skills-gradient'
+    >
+      {/* Section Title  */}
+      <div className='text-center mb-8'>
+        <h2 className='text-3xl sm:text-4xl font-bold text-white'>SKILLS</h2>
+        <div className='w-24 h-1 bg-[#8245ec] mx-auto mt-2'></div>
+      </div>
+
+    </section>
   )
 }
 

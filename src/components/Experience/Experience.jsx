dd90f5f61efa9e5698fb47f808496e9ab7e1cb8a
chaitanya197803 <cgarg9432@gmail.com>
2025-07-19 20:17:48 +0530

    EXPERIENCE

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { experiences } from '../../constant'
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+    id='experience'
+    className='py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient'
+    >
+      {/* Section Title  */}
+      <div className='text-center mb-16 text-white'>
+        <h2>EXPERIENCE</h2>
+      </div>
+
+    </section>
   )
 }
 

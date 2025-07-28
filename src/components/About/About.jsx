5bbb0554c07c49960bf8d69eb77195d0fff0ef77
chaitanya197803 <cgarg9432@gmail.com>
2025-07-29 01:47:31 +0530

    lottie image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -76,13 +76,13 @@ const About = () => {
 
         {/* Right Side  */}
             <div className='md:w-1/2 flex justify-center md:justify-end'>
-              <img 
+              {/* <img 
               src={aboutImage} 
               alt="Chaitanya Garg" 
               className='w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'
-              />
-              {/* <Lottie animationData={codeTyping} loop={true} 
-              className='w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'/> */}
+              /> */}
+              <Lottie animationData={codeTyping} loop={true} 
+              className='w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'/>
             </div>
 
             
